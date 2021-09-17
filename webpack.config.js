b1ecbf1253ec527f27381fe6e78c0b5fb85eb2e3
AndresDoncel <andresdoncel95@hotmail.com>
2021-09-17 16:23:01 -0500

    adjust webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "index_bundle.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        use: ["babel-loader"],
-      },
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === "production";
+  const isDevelopment = !isProduction;
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "index_bundle.js",
+    },
+    devtool: isDevelopment && "cheap-module-source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: ["babel-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  };
 };
